063aecba354dc15fbc9567c5530d560780b70986
ballicipluck <shyam4564@gmail.com>
2021-11-22 12:27:54 +0530

    10

--- gitter.bat
+++ gitter.bat
@@ -1,6 +1,6 @@
 git add .
-git commit -m "9"
+git commit -m "10"
 git remote remove origin
-git remote add origin https://test4564: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@github.com/test4564/main9.git
+git remote add origin https://test4564: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@github.com/test4564/main10.git
 git branch -M main
 git push -u origin main
